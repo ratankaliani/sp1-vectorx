88ed6e3eba45d96f7dd0ad9df0c1b26b17f12f85
Xavier D'Mello <xaviermdmello@gmail.com>
2024-05-22 17:37:10 -0700

    finish refactor fr

--- script/src/bin/header_range.rs
+++ script/src/bin/header_range.rs
@@ -1,35 +1,45 @@
 //! A simple script to generate and verify the proof of a given program.
-
 use crypto::{blake2b::Blake2b, digest::Digest};
-
 use codec::Encode;
 use sp1_sdk::{utils::setup_logger, ProverClient, SP1Stdin};
 use sp1_vectorx_primitives::types::HeaderRangeProofRequestData;
 use sp1_vectorx_script::input::RpcDataFetcher;
 use subxt::config::Header;
 
-const HEADER_RANGE_ELF: &[u8] =
-    include_bytes!("../../../header-range/elf/riscv32im-succinct-zkvm-elf");
+const HEADER_RANGE_ELF: &[u8] = include_bytes!("../../../header-range/elf/riscv32im-succinct-zkvm-elf");
 
-#[tokio::main]
-async fn main() {
-    setup_logger();
-
-    let fetcher = RpcDataFetcher::new().await;
-
-    // TODO: Update this to read from args/on-chain.
-    // let head = fetcher.get_head().await;
-    let trusted_block = 237600;
-    let target_block = 237960;
-
+async fn get_header_range_proof_request_data(
+    fetcher: &RpcDataFetcher,
+    trusted_block: u32,
+    target_block: u32,
+) -> HeaderRangeProofRequestData {
     let trusted_header = fetcher.get_header(trusted_block).await;
     let trusted_header_hash = trusted_header.hash();
-    
     let (authority_set_id, authority_set_hash) = fetcher
         .get_authority_set_data_for_block(trusted_block)
         .await;
 
-    // TODO: It may make sense to fetch this from an indexer similar to VectorX, this isn't resilient to downtime.
+    let num_headers = target_block - trusted_block + 1;
+    let merkle_tree_size = fetcher.get_merkle_tree_size(num_headers);
+
+    HeaderRangeProofRequestData {
+        trusted_block,
+        target_block,
+        trusted_header_hash: trusted_header_hash.0,
+        authority_set_hash: authority_set_hash.0,
+        authority_set_id,
+        merkle_tree_size,
+    }
+}
+
+async fn generate_and_verify_proof(
+    trusted_block: u32,
+    target_block: u32,
+) -> anyhow::Result<()> {
+    let fetcher = RpcDataFetcher::new().await;
+
+    let request_data = get_header_range_proof_request_data(&fetcher, trusted_block, target_block).await;
+
     let (target_justification, _) = fetcher.get_justification_data_for_block(target_block).await;
 
     let headers = fetcher
@@ -37,29 +47,19 @@ async fn main() {
         .await;
     let encoded_headers: Vec<Vec<u8>> = headers.iter().map(|header| header.encode()).collect();
 
-    let num_headers = target_block - trusted_block + 1;
-    let merkle_tree_size = fetcher.get_merkle_tree_size(num_headers);
-
     // Generate proof.
     let mut stdin = SP1Stdin::new();
-    stdin.write(&HeaderRangeProofRequestData {
-        trusted_block,
-        target_block,
-        trusted_header_hash: trusted_header_hash.0,
-        authority_set_hash: authority_set_hash.0,
-        authority_set_id,
-        merkle_tree_size,
-    });
+    stdin.write(&request_data);
+
     // Should be target_block - trusted_block + 1 headers.
     for encoded_header in encoded_headers {
         stdin.write_vec(encoded_header);
     }
-
     stdin.write(&target_justification);
 
     let client = ProverClient::new();
     let (pk, vk) = client.setup(HEADER_RANGE_ELF);
-    let mut proof = client.prove(&pk, stdin).expect("proving failed");
+    let mut proof = client.prove(&pk, stdin)?;
 
     // Read outputs.
     let mut state_root_commitment = [0u8; 32];
@@ -68,12 +68,24 @@ async fn main() {
     proof.public_values.read_slice(&mut data_root_commitment);
 
     // Verify proof.
-    client.verify(&proof, &vk).expect("verification failed");
+    client.verify(&proof, &vk)?;
 
     // Save proof.
-    proof
-        .save("proof-with-io.json")
-        .expect("saving proof failed");
+    proof.save("proof-with-io.json")?;
 
-    println!("successfully generated and verified proof for the program!")
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    setup_logger();
+
+    let trusted_block = 237600;
+    let target_block = 237960;
+
+    generate_and_verify_proof(trusted_block, target_block).await?;
+
+    println!("Successfully generated and verified proof for the program!");
+
+    Ok(())
 }
