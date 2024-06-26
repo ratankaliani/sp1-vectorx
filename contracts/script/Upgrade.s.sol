33ff65bbbb30db5e64ef7af132c0394d0f79019f
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-06-26 21:04:57 +0200

    fix: contracts and new render.yaml for operators

--- contracts/script/Upgrade.s.sol
+++ contracts/script/Upgrade.s.sol
@@ -10,9 +10,8 @@ import {SP1Verifier} from "@sp1-contracts/SP1Verifier.sol";
 import {SP1MockVerifier} from "@sp1-contracts/SP1MockVerifier.sol";
 import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
 import {SP1Vector} from "../src/SP1Vector.sol";
-import {ERC1967Proxy} from "@openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
 import {TimelockedUpgradeable} from "@succinctx/upgrades/TimelockedUpgradeable.sol";
-import {UUPSUpgradeable} from "@openzeppelin/proxy/utils/UUPSUpgradeable.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract DeployScript is Script {
     using stdJson for string;
@@ -27,7 +26,7 @@ contract DeployScript is Script {
         SP1Vector sp1VectorImpl = new SP1Vector{salt: bytes32(create2Salt)}();
 
         address existingProxyAddress = vm.envAddress("CONTRACT_ADDRESS");
-        UUPSUpgradeable proxy = UUPSUpgradeable(existingProxyAddress);
+        TimelockedUpgradeable proxy = TimelockedUpgradeable(existingProxyAddress);
         proxy.upgradeTo(address(sp1VectorImpl));
 
         SP1Vector sp1Vector = SP1Vector(address(existingProxyAddress));
