28a29d82026f753d5a17bcb278b07c56528ee1f1
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-06-26 20:26:30 +0200

    wip

--- contracts/script/Upgrade.s.sol
+++ contracts/script/Upgrade.s.sol
@@ -11,11 +11,11 @@ import {SP1MockVerifier} from "@sp1-contracts/SP1MockVerifier.sol";
 import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
 import {SP1Vector} from "../src/SP1Vector.sol";
 import {ERC1967Proxy} from "@openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
+import {TimelockedUpgradeable} from "@succinctx/upgrades/TimelockedUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/proxy/utils/UUPSUpgradeable.sol";
 
 contract DeployScript is Script {
     using stdJson for string;
-
-    ISP1Verifier public verifier;
 
     function setUp() public {}
 
@@ -24,16 +24,23 @@ contract DeployScript is Script {
 
         bytes32 create2Salt = bytes32(vm.envBytes("CREATE2_SALT"));
 
-        SP1Vector sp1VectorImpl = SP1Vector{salt: bytes32(create2Salt)}(0x0);
+        SP1Vector sp1VectorImpl = new SP1Vector{salt: bytes32(create2Salt)}();
 
         address existingProxyAddress = vm.envAddress("CONTRACT_ADDRESS");
-        ERC1967Proxy proxy = ERC1967Proxy(existingProxyAddress);
+        UUPSUpgradeable proxy = UUPSUpgradeable(existingProxyAddress);
         proxy.upgradeTo(address(sp1VectorImpl));
 
         SP1Vector sp1Vector = SP1Vector(address(existingProxyAddress));
 
+        SP1Verifier verifier = new SP1Verifier();
+        SP1MockVerifier mockVerifier = new SP1MockVerifier();
+
         // Update the SP1 Verifier address and the program vkey.
-        sp1Vector.updateVerifier(address(verifier));
+        if (vm.envBool("MOCK")) {
+            sp1Vector.updateVerifier(address(mockVerifier));
+        } else {
+            sp1Vector.updateVerifier(address(verifier));
+        }
         sp1Vector.updateVectorXProgramVkey(vm.envBytes32("SP1_VECTOR_PROGRAM_VKEY"));
 
         return address(existingProxyAddress);
